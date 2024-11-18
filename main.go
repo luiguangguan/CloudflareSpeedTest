bdff67d2188742edf766a50049a7b61757a31541
Simon <lvguangguan@live.com>
2024-11-18 16:07:03 +0800

    測試4.8

--- main.go
+++ main.go
@@ -204,7 +204,9 @@ func main() {
 		c := cron.New(cron.WithSeconds())
 
 		// 配置 cron 表达式
-		_, err2 := c.AddFunc(cronExpr, TestSpeed)
+		_, err2 := c.AddFunc(cronExpr, func{
+			fmt.Println("task starting")
+		})
 		if err2 != nil {
 			fmt.Println("Error adding cron job:", err2)
 			return
